068b4ac6c1c87fe2618e32e37ca7a84168c665be
Wei_Y <wei.yang@yuneec.com>
2018-03-27 11:07:44 +0800

    init reposity and first commit hisiv300libs repo

---
+++ glib-2.22.5/share/gdb/auto-load/libglib-2.0.so.0.2200.5-gdb.py
@@ -0,0 +1,10 @@
+import sys
+import gdb
+
+# Update module path.
+dir = '/home/yf/hisiv300Libs/glib-2.22.5/share/glib-2.0/gdb'
+if not dir in sys.path:
+    sys.path.insert(0, dir)
+
+from glib import register
+register (gdb.current_objfile ())
